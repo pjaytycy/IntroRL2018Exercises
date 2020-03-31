10ca9cada6084d76fab89349e79c4b2612fca286
Pieter-Jan Busschaert <pieter-jan.busschaert@exotecsolutions.com>
2020-03-31 16:14:45 +0200

    Add UpperConfidenceBound action selection next to e-greedy selection.
    The selection change works for all 3 types of action-value estimates. 
    Either provide explore_pct for e-greedy selection or confidence_level 
    for UCB selection. Note: you can use both at the same time, but that 
    doesn't make a lot of sense...

--- ch02_multi_armed_bandit/ex02.05_non_stationary/main.py
+++ ch02_multi_armed_bandit/ex02.05_non_stationary/main.py
@@ -40,12 +40,16 @@ class KArmedBandit:
 
 
 class Player:
-    def __init__(self, testbed, explore_pct):
+    def __init__(self, testbed, explore_pct, confidence_level = 0):
         self.testbed = testbed
         self.K = testbed.K
         self.explore_pct = explore_pct
+        self.confidence_level = confidence_level
         self.Q = numpy.zeros(self.K)
+        self.UCB = numpy.zeros(self.K)
+        self.N = numpy.zeros(self.K)
         self.Q_hist = [[] for i in range(self.K)]
+        self.UCB_hist = [[] for i in range(self.K)]
         self.r_hist = []
         self.optimal_choice = []
         self.debug = False
@@ -54,9 +58,9 @@ class Player:
     def play_one(self, t):
         k = self.select_action()
         r = self.testbed.do_action(k)
-        self.update(k, r)
+        self.update(k, r, t)
         if self.debug:
-            print("{} : r:{:6.3f} => Q[{}]={:6.3f}".format(k, r, k, self.Q[k]))
+            print("{} : r:{:6.3f} => Q[{}]={:6.3f}, UCB={}".format(k, r, k, self.Q[k], self.UCB))
         self.r_hist.append(r)
         self.optimal_choice.append((k == self.testbed._optimal_k) * 1.0)
 
@@ -74,8 +78,12 @@ class Player:
 
 
     def get_best_actions(self):
-        maxval = numpy.max(self.Q)
-        return list(numpy.argwhere(self.Q == maxval).flatten())
+        if self.confidence_level > 0:
+            N0 = list(numpy.argwhere(self.N == 0).flatten())
+            if len(N0) > 0:
+                return N0
+        maxval = numpy.max(self.UCB)
+        return list(numpy.argwhere(self.UCB == maxval).flatten())
 
 
     def select_random_action(self, possible_actions):
@@ -83,10 +91,17 @@ class Player:
         return possible_actions[i]
 
 
-    def update(self, k, r):
+    def update(self, k, r, t):
+        self.N[k] += 1
         self.update_action_value(k, r)
+        if self.confidence_level == 0:
+            self.UCB = self.Q
+        else:
+            self.UCB = self.Q + self.confidence_level * numpy.sqrt(numpy.log(t+1) / self.N)
+
         for i in range(self.K):
             self.Q_hist[i].append(self.Q[i])
+            self.UCB_hist[i].append(self.UCB[i])
 
 
     def update_action_value(self, k, r):
@@ -98,25 +113,25 @@ class Player:
         pyplot.title("evolution of Q estimates per arm")
         for k in range(self.K):
             self.Q_hist[k].append(self.testbed._q[k])
-            pyplot.plot(self.Q_hist[k], marker='o', markevery=[len(self.Q_hist[k])-1])
+            Qline, = pyplot.plot(self.Q_hist[k], marker='o', markevery=[len(self.Q_hist[k])-1])
+            if self.confidence_level > 0:
+                pyplot.plot(self.UCB_hist[k], linestyle=':', color=Qline.get_color())
 
 
 
 class SampleAveragePlayer(Player):
-    def __init__(self, testbed, explore_pct):
-        super().__init__(testbed, explore_pct)
-        self.N = numpy.zeros(self.K)
+    def __init__(self, testbed, explore_pct, confidence_level):
+        super().__init__(testbed, explore_pct, confidence_level)
 
 
     def update_action_value(self, k, r):
-        self.N[k] += 1
         self.Q[k] += 1.0/self.N[k] * (r - self.Q[k])
 
 
 
 class ConstantStepSizePlayer(Player):
-    def __init__(self, testbed, explore_pct, alpha):
-        super().__init__(testbed, explore_pct)
+    def __init__(self, testbed, alpha, explore_pct, confidence_level):
+        super().__init__(testbed, explore_pct, confidence_level)
         self.alpha = alpha
 
 
@@ -126,8 +141,8 @@ class ConstantStepSizePlayer(Player):
 
 
 class UnbiasedConstantStepSizePlayer(Player):
-    def __init__(self, testbed, explore_pct, alpha):
-        super().__init__(testbed, explore_pct)
+    def __init__(self, testbed, alpha, explore_pct, confidence_level):
+        super().__init__(testbed, explore_pct, confidence_level)
         self.alpha = alpha
         self.trace_of_one = numpy.zeros(self.K)
 
@@ -141,7 +156,7 @@ class UnbiasedConstantStepSizePlayer(Player):
 
 def main():
     num_arms = 10
-    num_runs = 2000
+    num_runs = 4000
     num_steps = 10000
 
     sigma_initial = 1.0
@@ -149,21 +164,29 @@ def main():
 
     explore_pct = 0.1
     fixed_alpha = 0.1
+    confidence_level = 2.0
 
     player_labels = []
-    player_labels.append("sample average")
-    player_labels.append("fixed alpha = {}".format(fixed_alpha))
-    player_labels.append("unbiased fixed alpha = {}".format(fixed_alpha))
+    player_labels.append("sample average + e-greedy {}".format(explore_pct))
+    player_labels.append("fixed alpha {} + e-greedy {}".format(fixed_alpha, explore_pct))
+    player_labels.append("unbiased fixed alpha {} + e-greedy {}".format(fixed_alpha, explore_pct))
+    player_labels.append("sample average + UCB {}".format(confidence_level))
+    player_labels.append("fixed alpha {} + UCB {}".format(fixed_alpha, confidence_level))
+    player_labels.append("unbiased fixed alpha {} + UCB {}".format(fixed_alpha, confidence_level))
+
     num_players = len(player_labels)
 
     avg_rewards = numpy.zeros((num_players, num_steps))
     avg_optimal = numpy.zeros((num_players, num_steps))
     for b in range(num_runs):
         testbed = KArmedBandit(num_arms, sigma_initial, sigma_random_walk)
-        player1 = SampleAveragePlayer(testbed, explore_pct)
-        player2 = ConstantStepSizePlayer(testbed, explore_pct, fixed_alpha)
-        player3 = UnbiasedConstantStepSizePlayer(testbed, explore_pct, fixed_alpha)
-        players = [player1, player2, player3]
+        players = []
+        players.append(SampleAveragePlayer(testbed, explore_pct, 0.0))
+        players.append(ConstantStepSizePlayer(testbed, fixed_alpha, explore_pct, 0.0))
+        players.append(UnbiasedConstantStepSizePlayer(testbed, fixed_alpha, explore_pct, 0.0))
+        players.append(SampleAveragePlayer(testbed, 0.0, confidence_level))
+        players.append(ConstantStepSizePlayer(testbed, fixed_alpha, 0.0, confidence_level))
+        players.append(UnbiasedConstantStepSizePlayer(testbed, fixed_alpha, 0.0, confidence_level))
         testbed.play(players, num_steps)
         for p, player in enumerate(players):
             avg_rewards[p] += numpy.asarray(player.r_hist)
